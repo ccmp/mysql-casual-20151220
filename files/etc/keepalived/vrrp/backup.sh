9d7217ddb06099735fa15d4fa66bbadb3423e3b2
Kimitoshi Takahashi <ktaka@ccmp.jp>
2015-11-15 21:40:41 +0900

    Some fixes.

--- files/etc/keepalived/vrrp/backup.sh
+++ files/etc/keepalived/vrrp/backup.sh
@@ -2,6 +2,6 @@
 
 TIMEOUT=2
 
-SOCK=/var/run/mysqld/mysql.sock
+SOCK=/var/run/mysqld/mysqld.sock
 mysql -S $SOCK --connect-timeout=$TIMEOUT -e "set @@global.read_only=1;"
 
--- files/etc/keepalived/vrrp/master.sh
+++ files/etc/keepalived/vrrp/master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOCK=/var/run/mysqld/mysql.sock
+SOCK=/var/run/mysqld/mysqld.sock
 mysql -S $SOCK -e "set @@global.read_only=0;"
 
 
