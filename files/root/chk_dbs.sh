5bc077cb6cd65a2b2239973a50ea6b528fa5efc7
Kimitoshi Takahashi <ktaka@ccmp.jp>
2015-11-16 03:12:25 +0900

    Add chk_db script

---
+++ files/root/chk_dbs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+HOSTS="v001 v002"
+
+for hst in $HOSTS; do
+
+echo
+
+uuid=$(mysql -u rep -prep  -h $hst -e 'select @@server_uuid;' 2>/dev/null |sed -e "/@@server_uuid/d" )
+readonly=$(mysql -u rep -prep  -h $hst -e "select @@global.read_only;" 2>/dev/null |sed -e "/@@global.read_only/d")
+
+echo -e $hst ":\tUUID=" $uuid ", read_only=" $readonly
+echo
+
+mysql -u rep -prep  -h $hst -e 'show slave status\G;' 2>/dev/null \
+|sed -e 'te' -e 'H;$!d;:e' -e 'x;/^$/d;s/,\n/,/g'|egrep -i 'gtid|Running|Behin'
+
+echo
+
+done
+
