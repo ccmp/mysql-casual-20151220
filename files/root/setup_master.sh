d5275e50b72c28c00682874b2c8bb61650ef6800
Kimitoshi Takahashi <ktaka@ccmp.jp>
2015-11-19 06:16:54 +0900

    Grant root accesss from remote host.

--- files/root/setup_master.sh
+++ files/root/setup_master.sh
@@ -5,6 +5,7 @@ SOCK=/var/run/mysqld/mysqld.sock
 echo "#### priv. settings for root, repl, test"
 mysql -S $SOCK -e "grant replication slave, replication client on *.* to 'rep'@'%' identified by 'rep';"
 mysql -S $SOCK -e "grant replication client on *.* to 'rep'@'localhost' identified by 'rep';"
+mysql -S $SOCK -e "grant all privileges on *.*  to 'root'@'%' identified by 'root' with grant option;"
 
 # Check priv.
 mysql -S $SOCK mysql -e "select Host, User from user where Host = '%' ;"
