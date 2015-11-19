e7263c9d2872efd79aedc4c96b8f77f61e949906
Kimitoshi Takahashi <ktaka@ccmp.jp>
2015-11-19 23:14:27 +0900

    Fix Xtrabackup  Scripts

--- files/root/xtrabackup.sh
+++ files/root/xtrabackup.sh
@@ -8,7 +8,7 @@ echo Backup Started at $(date)
 binlog=$(mysql -S $SOCK -e "flush logs;show master logs;"|tail -n 1 |sed -n 's/\(.*\.[0-9]\{6\}\).*/\1/p')
 mysql -S $SOCK -e "purge binary logs to '$binlog';"
 
-innobackupex --socket=$SOCK --user=root --slave-info --safe-slave-backup --defaults-file=/etc/mysql/my.cnf --stream=tar ./ | gzip - > xtrabackup.$(date +"%Y%m%d%H%M" ).tar.gz
+innobackupex --defaults-file=/etc/mysql/my.cnf --socket=$SOCK --user=root --slave-info --safe-slave-backup --stream=tar ./ | gzip - > xtrabackup.$(date +"%Y%m%d%H%M" ).tar.gz
 
 echo Backup Completed at $(date)
 
